72f019f83543e6d78bfe8a504d17539ffb405429
marcelocodigos <marcelofdsantoscode@gmail.com>
2023-11-27 00:30:53 -0300

    add function pergunta final

--- scripts/script.js
+++ scripts/script.js
@@ -12,10 +12,40 @@ document.getElementById("etapa-1").addEventListener("click",function(){
     
 });
 
-
+var perguntaAtual =0 
 continuarEtapa2=()=>{
     const etapa2 = document.querySelector(".etapa-2");
     etapa2.classList.toggle("remove");
     const etapa3 = document.querySelector(".etapa-3");
-    etapa3.classList.toggle("remove");
+    etapa3.classList.toggle("remove"); 
+    montaPerguntas();
+   
+}
+
+
+    
+montaPerguntas=()=>{
+    
+    var perguntaTEXT = document.querySelector("#perguntaAtual");
+    var perguntaTEXTPaginacao = document.querySelector("#texto-pergunta-atual");
+    perguntaTEXTPaginacao.textContent = `Pergunta ${perguntaAtual+1} de ${instrutoresPerguntas.perguntas.length}`;
+    perguntaTEXT.textContent =`${instrutoresPerguntas.perguntas[perguntaAtual].pergunta}`;
+    perguntaAtual++
+    console.log(perguntaAtual)
+    if(perguntaAtual==instrutoresPerguntas.perguntas.length){
+        perguntaFinal();
+    }
+   
+}
+perguntaFinal=()=>{
+    //perguntaAtualFinal
+    
+    const etapa3 = document.querySelector(".etapa-3");
+    const etapa4 = document.querySelector(".etapa-4");
+    etapa3.classList.toggle("remove"); 
+    etapa4.classList.toggle("remove");
+
+    var perguntaFinal = document.getElementById("perguntaAtualFinal");
+    perguntaFinal.textContent =`${instrutoresPerguntas.perguntas[perguntaAtual-1].pergunta}`;
+    
 }
