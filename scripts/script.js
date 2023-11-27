162ef88d8fa98fc8876fcae093138687cf1ec762
marcelocodigos <marcelofdsantoscode@gmail.com>
2023-11-26 23:39:01 -0300

    add new css in style.css

--- scripts/script.js
+++ scripts/script.js
@@ -5,29 +5,17 @@ document.getElementById("etapa-1").addEventListener("click",function(){
      const etapa1 = document.querySelector(".etapa-1")
      etapa2.classList.toggle("remove");
      etapa1.classList.toggle("remove");
-
-     // Certifique-se de que o script é executado após ter os dados disponíveis
+// Certifique-se de que o script é executado após ter os dados disponíveis
 // Certifique-se de que o elemento com ID "selectInstrutores" existe na sua página HTML
-
-const selectInstrutor = document.getElementById("selectInstrutores");
-selectInstrutor.innerHTML = "";
-
-for (let i = 0; i < instrutoresPerguntas.instrutores.length; i++) {
-  if (instrutoresPerguntas.instrutores[i].acao === 'Ativo') {
-    const nome = instrutoresPerguntas.instrutores[i].nome;
-    const option = document.createElement("option");
-    option.value = nome; // valor da opção é o nome do instrutor
-    option.text = nome;  // texto visível na opção é também o nome do instrutor
-    selectInstrutor.appendChild(option);
-  }
-}
-
 
    
     
 });
 
 
-// Certifique-se de que o script é executado após ter os dados disponíveis
-// Certifique-se de que o elemento com ID "selectInstrutores" existe na sua página HTML
-
+continuarEtapa2=()=>{
+    const etapa2 = document.querySelector(".etapa-2");
+    etapa2.classList.toggle("remove");
+    const etapa3 = document.querySelector(".etapa-3");
+    etapa3.classList.toggle("remove");
+}
